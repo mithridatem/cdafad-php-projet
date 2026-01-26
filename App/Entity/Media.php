365f810817b2a08b3104c4a84e776a74d2df9bb0
mithridatem <mathieumithridate@adrar-formation.com>
2026-01-26 10:04:59 +0100

    feat : add Entity User + Media

---
+++ App/Entity/Media.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+class Media
+{
+    //Attributs
+    private ?int $id;
+    private string $url;
+    private string $alt;
+    private \DateTimeImmutable $createdAt;
+    private ?\DateTimeImmutable $updatedAt;
+
+    //Constructeur
+    public function __construct(
+        string $url,
+        string $alt,
+        \DateTimeImmutable $createdAt
+    )
+    {
+        $this->url = $url;
+        $this->alt = $alt;
+        $this->createdAt = $createdAt;
+    }
+
+    //Getters et Setters
+    public function getId():?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id):self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function getAlt(): ?string
+    {
+        return $this->alt;
+    }
+
+    public function setAlt(?string $alt): self
+    {
+        $this->alt = $alt;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+        return $this;
+    }
+}
