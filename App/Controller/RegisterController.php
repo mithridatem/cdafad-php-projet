6321de65b3752b8aab9f0b02b6904acc95aa9155
mithridatem <mathieumithridate@adrar-formation.com>
2026-01-27 11:35:00 +0100

    feat : refacto RegisterContoller add couche SecurityService

--- App/Controller/RegisterController.php
+++ App/Controller/RegisterController.php
@@ -3,71 +3,29 @@
 namespace App\Controller;
 
 use App\Controller\AbstractController;
-use App\Utils\Tools;
-use App\Repository\UserRepository;
-use App\Entity\User;
-use App\Entity\Entity;
+use App\Service\SecurityService;
 
 class RegisterController extends AbstractController
 {
-    private UserRepository $userRepository;
+    private SecurityService $securityService;
 
     //Injection du UserRepository
     public function __construct()
     {
-        $this->userRepository = new UserRepository();
+        $this->securityService = new SecurityService();
     }
 
     //Méthode pour s'inscrire
     public function register(): mixed
     {
         $data = [];
+        
         //Test si le formulaire est submit
         if ($this->isFormSubmitted($_POST,  "submit")) {
-            //Test si les champs sont remplis
-            if (
-                !empty($_POST["pseudo"]) &&
-                !empty($_POST["email"]) &&
-                !empty($_POST["password"]) &&
-                !empty($_POST["confirm-password"])
-            ) {
-                //test si les 2 mots de passe sont identiques
-                if ($_POST["password"] == $_POST["confirm-password"]) {
-                    //Nettoyage des données
-                    Tools::sanitize_array($_POST);
-                    //créer un objet User
-                    $user = new User();
-                    //Set des attributs
-                    $user
-                        ->setEmail($_POST["email"])
-                        ->setPseudo($_POST["pseudo"])
-                        ->setFirstname($_POST["firstname"])
-                        ->setLastname($_POST["lastname"])
-                        ->setCreatedAt(new \DateTimeImmutable())
-                        ->setRoles("ROLE_USER");
-                    //Test si le compte n'existe pas déja
-                    if (!$this->userRepository->isUserExists($_POST["email"], $_POST["pseudo"])) {
-                        //Hash du passwords
-                        $hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-                        $user->setPassword($hash);
-
-                        //ajout en BDD
-                        $this->userRepository->save($user);
-                        $data["msg"] = "Le compte a été ajouté en BDD";
-                    } else {
-                        $data["msg"] = "Le compte existe déjà en BDD";
-                    }
-                }
-                //Sinon les champs ne sont pas identiques
-                else {
-                    $data["msg"] = "Les mots de passe ne sont pas identiques";
-                }
-            }
-            //Sinon les champs ne sont pas remplis 
-            else {
-                $data["msg"] = "Veuillez remplir les champs du formulaire";
-            }
+            //Ajout du compte en BDD
+            $data["msg"] = $this->securityService->saveUser($_POST);
         }
+
         return $this->render("register", "S'inscrire", $data);
     }
 
@@ -75,36 +33,13 @@ class RegisterController extends AbstractController
     public function login(): mixed
     {
         $data = [];
+
         //Test si le formulaire est soumis
         if ($this->isFormSubmitted($_POST)) {
-            //Test si les champs sont remplis
-            if (!empty($_POST["email"]) && !empty($_POST["password"])) {
-                //Nettoyer les entrées utilisateurs
-                Tools::sanitize_array($_POST);
-                //Récupérer le compte user
-                $user = $this->userRepository->findByEmail($_POST["email"]);
-                //Test si le compte existe
-                if (isset($user)) {
-                    //test le password
-                    if (password_verify($_POST["password"], $user->getPassword())) {
-                        //Créer la session User
-                        $_SESSION["user"] = [
-                            "id" => $user->getId(),
-                            "email"=> $user->getEmail(),
-                            "pseudo"=> $user->getEmail(),
-                            "roles" => $user->getRoles()
-                        ];
-                        $data["msg"] = "Connecté";
-                    } else {
-                        $data["msg"] = "Les informations de connexion sont invalides";
-                    }
-                } else {
-                    $data["msg"] = "Les informations de connexion sont invalides";
-                }
-            } else {
-                $data["msg"] = "Veuillez remplir tous les champs du formulaire";
-            }
+            //Logique de la connexion
+            $data["msg"] = $this->securityService->authenticate($_POST);
         }
+
         return $this->render("login", "Se connecter", $data);
     }
 
